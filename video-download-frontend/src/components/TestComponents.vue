2292d4734b3d030e47446b055907140410aa42fc
shiyiwan97 <1057862180@qq.com>
2023-03-23 18:03:33 +0800

    git init frontend

---
+++ video-download-frontend/src/components/TestComponents.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="hello">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'hello vue!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
